e94a2cfcee32ed0c9f669917d8abbced83da83eb
Valentina Fioravante <107444154+valenfz@users.noreply.github.com>
2024-07-04 18:07:11 -0300

    comentarios

--- src/router/report.router.js
+++ src/router/report.router.js
@@ -1,8 +1,10 @@
+// ROTA DE RELATÓRIOS
+
 const express = require('express');
 const router = express.Router();
 const reportController = require('../controller/report.controller');
 
-router.get('/reports/customer/:customerId/orders', reportController.findOrdersByCustomer);
-router.get('/ordersByStatus', reportController.findOrdersByStatus);
+router.get('/reports/customer/:customerId/orders', reportController.findOrdersByCustomer); //Chama o controlador para buscar pedidos de um cliente especifico
+router.get('/ordersByStatus', reportController.findOrdersByStatus); // Chama o controlador para buscar pedidos em aberto
 
 module.exports = router;
